c8fd1a2133c00c51a55c602c35ff9cba3c2e0253
NguyenVanTuan10120 <tuanguyen929@gmail.com>
2023-06-09 22:01:55 +0700

    validate form

---
+++ js/validate.js
@@ -0,0 +1,72 @@
+var password = document.querySelector(".password");
+var eyeOpen = document.querySelector(".eye-open");
+var eyeClose = document.querySelector(".eye-close");
+var email = document.querySelector(".email");
+var form = document.querySelector(".form");
+
+eyeOpen.addEventListener("click", function () {
+  eyeOpen.classList.add("hidden");
+  eyeClose.classList.remove("hidden");
+  password.setAttribute("type", "password");
+});
+eyeClose.addEventListener("click", function () {
+  eyeClose.classList.add("hidden");
+  eyeOpen.classList.remove("hidden");
+  password.setAttribute("type", "text");
+});
+
+function showError(input, message) {
+  var parent = input.parentElement;
+  var small = parent.querySelector("small");
+  parent.classList.add("error");
+  small.innerText = message;
+}
+
+function showSuccess(input) {
+  var parent = input.parentElement;
+  console.log(parent);
+  var small = parent.querySelector("small");
+  parent.classList.remove("error");
+  small.innerText = "";
+}
+
+function checkEmptyError(listInput) {
+  var isEmptyError = false;
+  listInput.forEach((input) => {
+    input.value = input.value.trim();
+    if (!input.value) {
+      isEmptyError = true;
+      showError(input, "Vui lòng nhập thông tin");
+    } else {
+      showSuccess(input);
+    }
+  });
+  return isEmptyError;
+}
+
+function checkEmailError() {
+  const regexEmail =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  input.value = input.value.trim();
+  var isEmailError = !regexEmail.test(input.value);
+  if (regexEmail.test(input.value)) {
+    showSuccess(input);
+  } else {
+    showError(input, "Email không hợp lệ");
+  }
+  return isEmailError;
+}
+function checkLengthError(input, min) {
+  input.value = input.value.trim();
+  if (input.value.Length < min) {
+    showError(input, `phải có ít nhất ${min} ký tự`);
+    return true;
+  }
+  return false;
+}
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  var isPasswordLength = checkLengthError(password, 6);
+  var isEmptyError = checkEmptyError([email, password]);
+  var isEmailError = checkEmailError(email);
+});
